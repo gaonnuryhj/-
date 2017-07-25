6edb497014a226bde6c4744102a9f51e8da1eb11
hyunjung <gaonnury_hj@naver.com>
2017-07-25 14:02:18 +0900

    트리 0725

--- 트리/트리/BinaryTree.cpp
+++ 트리/트리/BinaryTree.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 #define Max 100
+
 
 class Tree {
 	class Node {
@@ -9,17 +11,18 @@ class Tree {
 		Node* leftchild;
 		Node* rightchild;
 	};
+public:
 	Node* root = new Node;
 	int depth=1;
 	int num = 1;
 	Node* stack[Max];
 
-public:
+
 	Tree();
 	void Insert(int data);
 	Node* Search();
-	void Search_Inorder(int data);
-	void Search_Preorder(int data);
+	void Inorder(Node* temp);
+	void Preorder(Node* temp);
 	void Delete();
 	void PrintTree();
 
@@ -37,15 +40,25 @@ Tree::Tree() {
 
 Tree::Node* Tree::Search() {
 	Node* temp = root;
-	Node* stack[Max];
-	stack[0] = root;
-
-	while (temp->leftchild != NULL) {
-		
-
+	Node* newNode = new Node;
+	newNode->data = NULL;
+	newNode->leftchild = NULL;
+	newNode->rightchild = NULL;
+	queue<Node*> queue;
 	
+	while (temp->leftchild != NULL && temp->rightchild != NULL ) {
+		queue.push(temp->leftchild);
+		queue.push(temp->rightchild);
+		temp = queue.front();
+		queue.pop();		
 	}
 
+	if (temp->leftchild == NULL)
+		temp->leftchild = newNode;
+	else
+		temp->rightchild = newNode;
+		
+	return newNode;
 
 }
 
@@ -54,17 +67,80 @@ void Tree::Insert(int data) {
 	if (root->data == NULL) {
 		root->data = data;
 	}
-	Node* newNode = new Node;
-	newNode->data = data;
-	newNode->leftchild = NULL;
-	newNode->rightchild = NULL;
+	else {
+
+		Node* newNode = Search();
+		newNode->data = data;	
+	}
+}
+
+void Tree::PrintTree() {
 
 	Node* temp = root;
+	queue<Node*> queue;
+	queue.push(temp);
 
-	for (temp; temp->leftchild == NULL; temp = temp->leftchild) {
+	while (!queue.empty()) {
+
+		temp = queue.front();
+		queue.pop();
+
+		num++;
+		if (num % 2 ^ depth == 0) {
+			depth++;
+			cout << endl;
+		}
+
+		cout <<temp->data << "   ";
+
+		if(temp->leftchild!=NULL)
+			queue.push(temp->leftchild);
+		if(temp->rightchild!=NULL)
+			queue.push(temp->rightchild);
 		
 	}
-	
+
+}
+
+void Tree::Inorder(Node* temp) {
+
+	cout << temp->data<< "   ";
+
+	if (temp->leftchild != NULL)
+		Inorder(temp->leftchild);
+
+	if (temp->rightchild != NULL)
+		Inorder(temp->rightchild);
+
+}
+
+void Tree::Preorder(Node* temp) {	
+
+	if (temp->leftchild != NULL)
+		Preorder(temp->leftchild);
+
+	cout << temp->data << "   ";
+
+	if (temp->rightchild != NULL)
+		Preorder(temp->rightchild);
+
+}
 
 
+void main() {
+
+	Tree tree;
+
+	tree.Insert(1);
+	tree.Insert(2);
+	tree.Insert(3);
+	tree.Insert(4);
+	tree.Insert(5);
+	tree.PrintTree();
+	cout << endl;
+
+	tree.Preorder(tree.root);
+	cout << endl;
+
+	tree.Inorder(tree.root);
 }
